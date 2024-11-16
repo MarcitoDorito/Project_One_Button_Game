785f264d7c1915769aa6661ad0a3bc92a9a747e4
MarcitoDorito <400043361@st.roc.a12.nl>
2024-11-16 23:12:59 +0100

    Cutting of the blocks and speeding up coded

--- Stack_Up_Game/Assets/Scripts/BlockMovement.cs
+++ Stack_Up_Game/Assets/Scripts/BlockMovement.cs
@@ -6,7 +6,14 @@ public class BlockMovement : MonoBehaviour
 {
     public TowerScript tower;
     private float blockTransition = 0.0f;
-    private float blockSpeed = 2.5f;
+
+    private float blockSpeed = 2.0f;
+
+    private float speedUp = 0.5f;
+
+    public int speedUpCounter = 0;
+
+    //public bool isPlacedOnX = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +23,32 @@ public class BlockMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (speedUpCounter == 6) 
+        {
+            blockSpeed += speedUp;
+
+            speedUpCounter = 0;
+
+            Debug.Log(speedUpCounter);
+        }
+
     }
 
     public void moveBlock()
     {
+
+        if (tower.isGameOver)
+        {
+            return;
+        }
         blockTransition += Time.deltaTime * blockSpeed;
-        tower.theTower[tower.towerIndex].transform.localPosition = new Vector3(0,Mathf.Sin(blockTransition) * TowerScript.BLOCK_SIZE,0);
+        //if (isPlacedOnX)
+        //{
+        tower.theTower[tower.towerIndex].transform.localPosition = new Vector2(Mathf.Sin(blockTransition) * TowerScript.BLOCK_SIZE, tower.scoreCount);
+        //}
+        //else
+        //{
+        //    tower.theTower[tower.towerIndex].transform.localPosition = new Vector3(0, Mathf.Sin(blockTransition) * TowerScript.BLOCK_SIZE, tower.secondaryPosition);
+        //}
     }
 }
--- Stack_Up_Game/Assets/Scripts/TowerScript.cs
+++ Stack_Up_Game/Assets/Scripts/TowerScript.cs
@@ -1,17 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 public class TowerScript : MonoBehaviour
 {
     public BlockMovement blockMovement;
+
     public const float BLOCK_SIZE = 7.0f;
 
+    private const float TOWER_MOVE_SPEED = 5.0f;
+
+    private const float ERROR_MARGIN = 0.1f;
+
     public GameObject[] theTower;
+    private Vector2 towerBounds = new Vector2(BLOCK_SIZE,0);
 
     public int towerIndex;
 
     public int scoreCount = 0;
+
+    private int comboCount = 0;
+
+    public float secondaryPosition;
+
+    private Vector2 towerPosition;
+
+    private Vector3 lastBlockPlacement;
+
+    public bool isGameOver = false;
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -30,7 +48,7 @@ public class TowerScript : MonoBehaviour
         {
             if (placeBlock())
             {
-            intatiateBlock();
+            instatiateBlock();
             scoreCount++;   
             Debug.Log(scoreCount);
             }
@@ -38,27 +56,57 @@ public class TowerScript : MonoBehaviour
             {
                 EndGame();
             }
-            blockMovement.moveBlock();
         }
+            blockMovement.moveBlock();
+
+        transform.position = Vector3.Lerp(transform.position, towerPosition, TOWER_MOVE_SPEED * Time.deltaTime);
     }
-    private void intatiateBlock()
+    private void instatiateBlock()
     {
+        lastBlockPlacement = theTower[towerIndex].transform.localPosition;
         towerIndex--;
         if (towerIndex < 0)
         {
             towerIndex = transform.childCount - 1;
         }
+        towerPosition = Vector2.down * scoreCount;
         theTower[towerIndex].transform.localPosition = new Vector2(0, scoreCount);
+        theTower[towerIndex].transform.localScale = new Vector3(towerBounds.x, 1, towerBounds.y);
     }
 
 
     private bool placeBlock()
     {
-        return false;
+        Transform t = theTower[towerIndex].transform;
+        blockMovement.speedUpCounter++;
+
+        float deltaX = t.localPosition.x - lastBlockPlacement.x;
+        if (Mathf.Abs(deltaX) > ERROR_MARGIN)
+        {
+            comboCount = 0;
+            towerBounds.x -= Mathf.Abs(deltaX);
+            if (towerBounds.x < 0)
+            {
+                return false;
+            }
+            float middle = (lastBlockPlacement.x + t.localPosition.x) /2;
+            t.localScale = new Vector3(towerBounds.x, 1, towerBounds.y);
+            t.localPosition = new Vector3(middle, scoreCount, lastBlockPlacement.z);
+            /*            t.localPosition = new Vector3(middle - (lastBlockPlacement.x/2),scoreCount, lastBlockPlacement.z);*/
+        }
+        else
+        {
+            comboCount++;
+            t.localPosition = new Vector3(lastBlockPlacement.x, scoreCount, lastBlockPlacement.z);
+            
+        }
+        return true;
     }
 
     private void EndGame()
     {
         Debug.Log("Game Over");
+        theTower[towerIndex].AddComponent<Rigidbody2D>();
+        isGameOver = true;
     }
 }
